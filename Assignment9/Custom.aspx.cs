29e0447f80242db3f362728dccbb4a3d40e8a07d
Mittaanjali <mitta.anjali@mphasis.com>
2023-12-21 11:26:32 +0530

    assignment9

---
+++ Assignment9/Custom.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment9
+{
+    public partial class Customer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            LblInfo.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            LblInfo.Visible = true;
+            LblInfo.Text = "Customer Name : " + TextBox1.Text;
+            LblInfo.Text += "Mobile No : " + TextBox2.Text;
+            LblInfo.Text += "Email Id : " + TextBox3.Text;
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
